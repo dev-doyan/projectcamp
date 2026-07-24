046c002720c58f1cf0b46cbf0192f8432e35687a
doyan-dev <db9326300292@gmail.com>
2026-07-24 20:35:42 +0530

    coded project creation

--- src/controller/projectcontroller.js
+++ src/controller/projectcontroller.js
@@ -1,5 +1,39 @@
-export const createProject
-export const getAllprojects
-export const getProjectById
-export const updateProject
-export const deleteProject
+import db from "../db/index.js"
+import { projectTable } from "../models/projectModel.js";
+
+
+
+export const createProject=async(req,res)=>{
+    try {
+        const{name,description}=req.body
+        const uid=req.user.id;
+
+if(!uid){
+    return res.status(401).json({mssg:"user unidentified"})
+}
+
+if(!name){
+    return res.status(400).json({mssg:"need a name for tthe project "});
+}
+
+
+const project =await db.insert(projectTable).values({
+    name,
+    description: req.body.description ||null,
+    ownerId:uid
+}).returning({name:projectTable.name,
+    description:projectTable.description,
+    createdby:projectTable.ownerId
+})
+
+return res.status(200).json({mssg:"success",project})
+
+
+    } catch (error) {
+        return res.status(500).json({mssg:error.mssg})
+    }
+}
+// export const getAllprojects
+// export const getProjectById
+// export const updateProject
+// export const deleteProject
--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -2,17 +2,17 @@ import express from "express"
 const router=express.Router();
 import { authenticate } from "../middlewear/authenticate.js";
 
-import {createProject,getAllprojects,getAllprojects,updateProject,deleteProject} from "../controller/projectcontroller.js";
+import {createProject} from "../controller/projectcontroller.js";
 
 router.post("/", authenticate, createProject);
 
-router.get("/", authenticate, getAllProjects);
+// router.get("/", authenticate, getAllprojects);
 
-router.get("/:id", authenticate, getProjectById);
+// router.get("/:id", authenticate, getProjectById);
 
-router.patch("/:id", authenticate, updateProject);
+// router.patch("/:id", authenticate, updateProject);
 
-router.delete("/:id", authenticate, deleteProject);
+// router.delete("/:id", authenticate, deleteProject);
 
 
 export default router;
